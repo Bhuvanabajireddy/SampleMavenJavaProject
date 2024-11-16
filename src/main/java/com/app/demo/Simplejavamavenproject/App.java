acb231433fec7695e9643223e7bf7136f33d6f2d
Bhuvanabajireddy <sribhuvanabajireddy@gmail.com>
2024-11-16 15:56:15 +0530

    done

---
+++ src/main/java/com/app/demo/Simplejavamavenproject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Simplejavamavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
